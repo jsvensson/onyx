b3c4746ae9ca8809dc616eff6607f5a93a1fe24e
Johan Svensson <johan@atomicplayboy.net>
2016-06-14 22:53:56 +0200

    It renders!

---
+++ index.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Onyx } from './components/Onyx.jsx';
+
+ReactDOM.render(<Onyx />,  document.querySelector('#root'));
