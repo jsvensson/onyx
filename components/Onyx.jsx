77da3a3331410d5da3d50408fcc1101a494cade2
Johan Svensson <johan@atomicplayboy.net>
2016-06-14 23:21:49 +0200

    It's alive!

--- components/Onyx.jsx
+++ components/Onyx.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import { OnyxHeader } from './OnyxHeader.jsx'
+
 export class Onyx extends Component {
   render() {
     return(
       <div className="onyx root">
-        <h1>Onyx Contact Force</h1>
+        <OnyxHeader />
       </div>
     )
   }
